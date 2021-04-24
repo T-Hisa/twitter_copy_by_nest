b8659cc6cb11140a001539de303caae0c120c458
Takaaki <dacchin.h081229@gmail.com>
2021-04-24 21:14:30 +0900

    commit

--- webpack/server/webpack.prod.js
+++ webpack/server/webpack.prod.js
@@ -3,6 +3,13 @@ const commonConf = require('./webpack.common')
 
 // const outputFilename = '[hash]'
 const outputFilename = 'server'
+
 module.exports = webpackMerge(commonConf(outputFilename), {
-  mode: 'production'
+  mode: 'production',
+  // minimize: false にしておかないと、passport 周りでなぜかおかしくなる
+  // (具体的には、LocalStrategy と jwtStrategy の片方が読み込まれなくなる)
+  // ので、渋々minimize: false にしておく
+  optimization: {
+    minimize: false,
+  },
 })
